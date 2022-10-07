f2fd3e1375e42d724ab732f9096e9e09986062b7
Rich Sharp <richpsharp@gmail.com>
2022-10-07 10:59:24 -0700

    building target rasters

--- rasterize_landuse_polygons.py
+++ rasterize_landuse_polygons.py
@@ -3,7 +3,9 @@ import glob
 import logging
 import os
 
+
 from ecoshard import geoprocessing
+from ecoshard import taskgraph
 from osgeo import gdal
 import numpy
 
@@ -18,24 +20,49 @@ LOGGER = logging.getLogger(__name__)
 TARGET_PIXEL_SIZE = 10.0
 
 
+def get_all_field_values(shapefile_path, field_id):
+    """Return all values in field_id for all features in ``shapefile_path``."""
+    landcover_id_set = set()
+    vector = gdal.OpenEx(shapefile_path, gdal.OF_VECTOR)
+    layer = vector.GetLayer()
+    for feature in layer:
+        landcover_id_set.add(feature.GetField(landcover_field))
+    return landcover_id_set
+
+
 def main():
     path_to_shapefiles = './data/land_use_polygons/*'
+    path_to_target_rasters = './data/landcover_rasters/'
+    os.makedirs(path_to_target_rasters, exist_ok=True)
     landcover_field = 'AGG12'
     landcover_id_set = set()
+    field_value_task_list = []
     for shapefile_path in glob.glob(path_to_shapefiles):
-        LOGGER.info(f'processing {shapefile_path}')
         basename = os.path.basename(os.path.splitext(shapefile_path)[0])
+        task = task_graph.add_task(
+            func=get_all_field_values,
+            args=(shapefile_path, landcover_field),
+            store_result=True,
+            task_name=f'{landcover_field} values for {basename}')
+        field_value_task_list.append(task)
+
+        LOGGER.info(f'processing {shapefile_path}')
         vector_info = geoprocessing.get_vector_info(shapefile_path)
         xwidth = numpy.subtract(*[vector_info['bounding_box'][i] for i in (2, 0)])
         ywidth = numpy.subtract(*[vector_info['bounding_box'][i] for i in (3, 1)])
         n_cols = int(xwidth / TARGET_PIXEL_SIZE)
         n_rows = int(ywidth / TARGET_PIXEL_SIZE)
         LOGGER.info(f'expected raster size for {basename} is ({n_cols}x{n_rows})')
-        # vector = gdal.OpenEx(shapefile_path, gdal.OF_VECTOR)
-        # layer = vector.GetLayer()
-        # for feature in layer:
-        #     landcover_id_set.add(feature.GetField(landcover_field))
-    # LOGGER.info(f'landcover set: {landcover_id_set}')
+
+        target_raster_path = os.path.join(path_to_target_rasters, f'{basename}_lulc.tif')
+        geoprocessing.create_raster_from_vector_extents(
+            shapefile_path, target_raster_path, (TARGET_PIXEL_SIZE, -TARGET_PIXEL_SIZE),
+            gdal.GDT_Byte, 128,
+            raster_driver_creation_tuple=DEFAULT_GTIFF_CREATION_TUPLE_OPTIONS)
+
+    for task in field_value_task_list:
+        landcover_id_set |= task.get()
+    LOGGER.info(f'landcover set: {landcover_id_set}')
 
 
 if __name__ == '__main__':
