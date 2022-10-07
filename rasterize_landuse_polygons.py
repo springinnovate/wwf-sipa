b7c47c246d89456dfe8c5d30d404cd3d6de1dc09
Rich Sharp <richpsharp@gmail.com>
2022-10-07 10:37:09 -0700

    track rasterize landuse polygons

---
+++ rasterize_landuse_polygons.py
@@ -0,0 +1,31 @@
+"""Rasterize the landuse polygons onto a single raster."""
+import glob
+import logging
+
+from ecoshard import geoprocessing
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'))
+logging.getLogger('taskgraph').setLevel(logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    path_to_shapefiles = './data/land_use_polygons/*.shp'
+    landcover_field = 'AGG12'
+    landcover_id_set = set()
+    for shapefile_path in glob.glob(path_to_shapefiles):
+        LOGGER.info(f'processing {shapefile_path}')
+        vector = gdal.OpenEx(shapefile_path, gdal.OF_VECTOR)
+        layer = vector.GetLayer()
+        for feature in layer:
+            landcover_id_set.add(feature.GetField(landcover_field))
+    LOGGER.info(f'landcover set: {landcover_id_set}')
+
+
+
+if __name__ == '__main__':
+    main()
