f9e9056145f776b66ba8a57bb64b4e838d08b778
Rich Sharp <richpsharp@gmail.com>
2023-04-14 19:10:55 -0700

    fixing issue with killing / on path because of santizie

--- continuous_change_scenario_from_infra.py
+++ continuous_change_scenario_from_infra.py
@@ -350,9 +350,10 @@ def main():
         if row[GIS_TYPE_FIELD] == RASTER_TYPE:
             raster_path = row[PATH_FIELD]
             mask_value_str = row[RASTER_VALUE_FIELD]
-            local_path = sanitize_filename(os.path.join(
-                local_workspace, f'%s_{mask_value_str}%s' % os.path.splitext(
-                    os.path.basename(raster_path))))
+            local_path = os.path.join(
+                local_workspace, sanitize_filename(
+                    f'%s_{mask_value_str}%s' % os.path.splitext(
+                        os.path.basename(raster_path))))
             pressure_mask_raster_info[PATH_FIELD] = local_path
             _mask_raster(task_graph, raster_path, row, local_path)
         elif row[GIS_TYPE_FIELD] == VECTOR_TYPE:
