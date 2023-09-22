fc27d322eba116a134107e51bbccf731e6a7579e
Rich Sharp <richpsharp@gmail.com>
2023-09-22 11:46:35 -0700

    track changes to allow different scale

--- erosivity_calculator.py
+++ erosivity_calculator.py
@@ -64,95 +64,6 @@ DATASET_CRS = 'EPSG:4326'
 DATASET_SCALE = 27830
 
 
-def check_dataset_collection(model, dataset_id, band_id, start_year, end_year):
-    def band_checker(image):
-        available_bands = image.bandNames()
-        return ee.Image(ee.Algorithms.If(
-            available_bands.contains(band_id), image,
-            ee.Image.constant(0).rename('dummy_band')))
-
-    try:
-        collection = (
-            ee.ImageCollection(dataset_id)
-            .filter(ee.Filter.eq('model', model))
-            .filter(ee.Filter.calendarRange(start_year, end_year, 'year'))
-            .map(band_checker))
-
-        # Filter out images with the 'dummy_band'
-        collection = collection.filterMetadata(
-            'system:band_names', 'not_equals', ['dummy_band'])
-
-        size = collection.size().getInfo()
-        print(f'{model} has {size} elements with the band(s) {band_id}')
-        return size > 0
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
-
-
-def save_to_drive(
-        image, folder, description, scale, ee_poly, clip_poly_path,
-        target_raster_path):
-    ee.batch.Export.image.toDrive({
-        'image': image,
-        'description': description,
-        'folder': folder,
-        'scale': scale,
-        'crs': 'EPSG:4326',
-        'region': ee_poly.geometry().bounds(),
-        'fileFormat': 'GeoTIFF',
-        })
-    # url = image.toDrive({
-    #     'scale': scale,
-    #     'crs': 'EPSG:4326',
-    #     'region': ee_poly.geometry().bounds(),
-    #     'fileFormat': 'GeoTIFF',
-    #     'folder': 'gee_output',
-    #     'description': description,
-    # })
-
-    # response = requests.get(url)
-    # if response.status_code == 200:
-    #     content_type = response.headers.get('content-type')
-    #     if 'application/zip' in content_type or 'application/octet-stream' in content_type:
-    #         zip_buffer = io.BytesIO(response.content)
-    #         target_preclip = f"pre_clip_{description}.tif"
-
-    #         with zipfile.ZipFile(zip_buffer) as zip_ref:
-    #             zip_ref.extractall(f"{description}")
-    #             # Assuming there is only one tif file in the zip
-    #             for filename in zip_ref.namelist():
-    #                 if filename.endswith('.tif'):
-    #                     # Optionally rename the file
-    #                     if os.path.exists(target_preclip):
-    #                         os.remove(target_preclip)
-    #                     os.rename(f"{description}/{filename}", target_preclip)
-    #                     print(f"Successfully downloaded and unzipped {filename}")
-    #         r = gdal.OpenEx(target_preclip, gdal.OF_RASTER)
-    #         b = r.GetRasterBand(1)
-    #         b.SetNoDataValue(-9999)
-    #         b = None
-    #         r = None
-    #         raster_info = geoprocessing.get_raster_info(target_preclip)
-
-    #         geoprocessing.warp_raster(
-    #             target_preclip, raster_info['pixel_size'],
-    #             target_raster_path,
-    #             'near',
-    #             vector_mask_options={
-    #                 'mask_vector_path': clip_poly_path,
-    #                 'all_touched': True,
-    #                 })
-    #         os.remove(target_preclip)
-    #         LOGGER.info(f'saved {target_raster_path}')
-    #     else:
-    #         print(f"Unexpected content type: {content_type}")
-    #         print(response.content.decode('utf-8'))
-    # else:
-    #     print(f"Failed to download {description} from {url}")
-
-
 def authenticate():
     try:
         ee.Initialize()
@@ -189,6 +100,10 @@ def main():
         'Two date ranges in YYYY format to download between.'))
     parser.add_argument(
         '--status', action='store_true', help='To check task status')
+    parser.add_argument(
+        '--dataset_scale', type=float, default=DATASET_SCALE, help=(
+            f'Override the base scale of {DATASET_SCALE}m to '
+            f'whatever you desire.'))
     args = parser.parse_args()
     authenticate()
 
@@ -217,10 +132,6 @@ def main():
     start_year = int(args.date_range[0])
     end_year = int(args.date_range[1])
 
-    def filter_model(model):
-        return model if check_dataset_collection(
-            model, DATASET_ID, 'pr', start_year, end_year) else None
-
     cmip6_dataset = ee.ImageCollection(DATASET_ID).filter(
         ee.Filter.And(
             ee.Filter.inList('model', VALID_MODEL_LIST),
@@ -231,26 +142,28 @@ def main():
     description = (
         f'erosivity_{region_basename}_{args.scenario_id}_'
         f'{start_year}_{end_year}')
-    target_raster_path = f'{description}.tif'
 
     yearly_collection = cmip6_dataset.filter(
         ee.Filter.calendarRange(start_year, end_year, 'year'))
-
     erosivity_image = yearly_collection.reduce(
         ee.Reducer.sum()).multiply(86400/((end_year-start_year+1)*len(
             VALID_MODEL_LIST))).pow(1.1801).multiply(1.2718)
-
     erosivity_image_clipped = erosivity_image.clip(ee_poly)
+
     folder_id = 'gee_output'
-    save_to_drive(
-        erosivity_image_clipped,
-        folder_id,
-        description, DATASET_SCALE, ee_poly,
-        local_shapefile_path, target_raster_path)
+    ee.batch.Export.image.toDrive(
+        image=erosivity_image_clipped.resample('bilinear'),
+        description=description,
+        folder=folder_id,
+        scale=args.dataset_scale,
+        crs=DATASET_CRS,
+        region=ee_poly.geometry().bounds(),
+        fileFormat='GeoTIFF',
+        ).start()
 
     print(
-        f'downloaded erosivity raster to google drive: '
-        f'{folder_id}/{target_raster_path}')
+        f'downloading erosivity raster to google drive: '
+        f'{folder_id}/{description}')
 
 
 if __name__ == '__main__':
