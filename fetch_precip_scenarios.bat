698c306df9bbedc22a640d8b3df8618edb460c0d
Rich Sharp <richpsharp@gmail.com>
2023-10-06 11:24:47 -0700

    do the actual outline

--- fetch_precip_scenarios.bat
+++ fetch_precip_scenarios.bat
@@ -1,5 +1,5 @@
 set startYear = 2035
 set endYear = 2035
 set scenario = ssp245
-call python precip_calculator.py --aoi_vector_path ./data/ph_shape.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90
-call python n_events_calculator.py --aoi_vector_path ./data/ph_shape.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90
+call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/PH_outline.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90
+call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/PH_outline.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90
