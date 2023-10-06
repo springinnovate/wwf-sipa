41f436e40ae73bedb6dae6718fe7dabd1c09c412
Rich Sharp <richpsharp@gmail.com>
2023-10-06 11:42:03 -0700

    do ph and idn

--- fetch_precip_scenarios.bat
+++ fetch_precip_scenarios.bat
@@ -1,16 +1,19 @@
 set delayedexpansion true
-set startYear=2035
-set endYear=2065
-set scenario=ssp245
-set eventThreshold=10
 
-call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/PH_outline.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale 1000
-call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/PH_outline.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale 1000
+FOR A (PH_outline.gpkg IDN_outline.gpkg) DO (
+    set startYear=2040
+    set endYear=2060
+    set scenario=ssp245
+    set eventThreshold=10
 
-set startYear=1984
-set endYear=2014
-set scenario=historical
-set eventThreshold=10
+    call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale 1000
+    call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale 1000
 
-call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/PH_outline.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale 1000
-call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/PH_outline.gpkg --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale 1000
+    set startYear=1985
+    set endYear=2014
+    set scenario=historical
+    set eventThreshold=10
+
+    call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale 1000
+    call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale 1000
+)
