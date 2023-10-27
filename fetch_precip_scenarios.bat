61742911f0826ca581ea43ff89909126c6dcac3a
Rich Sharp <richpsharp@gmail.com>
2023-10-27 15:35:46 -0700

    track it

--- fetch_precip_scenarios.bat
+++ fetch_precip_scenarios.bat
@@ -10,8 +10,8 @@ set eventThreshold=10
 set datasetScale=1000
 
 FOR %%A IN (PH_outline.gpkg IDN_outline.gpkg) DO (
-    ECHO call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale datasetScale
-    ECHO call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale datasetScale
+    call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale %datasetScale%
+    call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale %datasetScale%
 )
 
 set startYear=2040
@@ -19,6 +19,6 @@ set endYear=2060
 set scenario=ssp245
 
 FOR %%A IN (PH_outline.gpkg IDN_outline.gpkg) DO (
-    ECHO call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale datasetScale
-    ECHO call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale datasetScale
+    call python precip_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --percentile 10 50 90 --dataset_scale %datasetScale%
+    call python n_events_calculator.py --aoi_vector_path ./data/admin_boundaries/%%A --scenario_id %scenario% --date_range %startYear% %endYear% --threshold %eventThreshold% --percentile 10 50 90 --dataset_scale %datasetScale%
 )
