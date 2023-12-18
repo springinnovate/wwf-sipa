68b1dcf6063e538418f9fb743a8a35cbdb5bcaed
Rich Sharp <richpsharp@gmail.com>
2023-12-18 12:11:46 -0800

    track postprocess script

---
+++ postprocess_cons_overlap_priority_areas.py
@@ -0,0 +1,25 @@
+"""
+Conservation priorities overlap analysis:
+1) how much of high value ES area (top 10% map) is outside of current
+   protected area (PAs) network? Then,
+2) overlap the areas of top 10% outside of the PA with KBAs and answer:
+    2a) How much of high value ES area would be priorities for protection or
+        restoration for ES?
+
+Desired output: maps, % values
+Inputs to use:
+PH PAs: './data/protected_areas/PH_Combined_PAs'
+IDN PAs: './data/protected_areas/ID_Combined PAs'
+IDN KBAs: './data/protected_areas/Indonesia_KBA'
+ https://drive.google.com/drive/folders/185VZCFjfeshqvTZzoZWfi2AH1avL7ShG?usp=drive_link
+ https://drive.google.com/drive/folders/1OmMBQFjLh9-jYb-qp7L7378AWCWdzhrN?usp=drive_link
+ https://drive.google.com/file/d/17dIK23oABdBQc7izVxxtwcRf38brDb4G/view?usp=drive_link
+"""
+
+
+def main():
+    """Entry point."""
+
+
+if __name__ == '__main__':
+    main()
