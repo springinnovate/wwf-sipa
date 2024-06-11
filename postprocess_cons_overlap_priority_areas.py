0d5ba7d5380b0c6a96bfc6304d731f18b9370032
Rich Sharp <richpsharp@gmail.com>
2024-06-11 09:50:11 -0700

    refactor to do km2 directly from the raster via reprojection

--- postprocess_cons_overlap_priority_areas.py
+++ postprocess_cons_overlap_priority_areas.py
@@ -58,6 +58,7 @@ SERVICE_OVERLAP_RASTERS = {
         r"D:\repositories\wwf-sipa\fig_generator_dir\overlap_rasters\overlap_combos_top_10_IDN_restoration_each ecosystem service.tif",
     ],
 }
+ELLIPSOID_EPSG = 6933
 
 RESULTS_DIR = f'workspace_{os.path.basename(os.path.splitext(__file__)[0])}'
 WORKING_DIR = os.path.join(RESULTS_DIR, 'working_dir')
@@ -65,52 +66,17 @@ for dir_path in [RESULTS_DIR, WORKING_DIR]:
     os.makedirs(dir_path, exist_ok=True)
 
 
-def area_of_pixel_km2(pixel_size, center_lat):
-    """Calculate km^2 area of a wgs84 square pixel.
-
-    Adapted from: https://gis.stackexchange.com/a/127327/2397
-
-    Parameters:
-        pixel_size (float): length of side of pixel in degrees.
-        center_lat (float): latitude of the center of the pixel. Note this
-            value +/- half the `pixel-size` must not exceed 90/-90 degrees
-            latitude or an invalid area will be calculated.
-
-    Returns:
-        Area of square pixel of side length `pixel_size` centered at
-        `center_lat` in km^2.
-
-    """
-    a = 6378137  # meters
-    b = 6356752.3142  # meters
-    e = numpy.sqrt(1 - (b/a)**2)
-    area_list = []
-    for f in [center_lat+pixel_size/2, center_lat-pixel_size/2]:
-        zm = 1 - e*numpy.sin(numpy.radians(f))
-        zp = 1 + e*numpy.sin(numpy.radians(f))
-        area_list.append(
-            numpy.pi * b**2 * (
-                numpy.log(zp/zm) / (2*e) +
-                numpy.sin(numpy.radians(f)) / (zp*zm)))
-    # mult by 1e-6 to convert m^2 to km^2
-    return pixel_size / 360. * (area_list[0] - area_list[1]) * 1e-6
-
-
 def main():
     """Entry point."""
-    #task_graph = taskgraph.TaskGraph(RESULTS_DIR, os.cpu_count()//2+2, 15.0)
     task_graph = taskgraph.TaskGraph(RESULTS_DIR, -1)
 
-    pixel_counts_per_region = {}
+    raster_area_per_region = {}
     for region_id in REGIONS_TO_ANALYZE:
         for service_overlap_raster_path in SERVICE_OVERLAP_RASTERS[region_id]:
             service_overlap_in_pa_path = os.path.join(
                 WORKING_DIR,
                 f'%s_{region_id}_in_pa%s' % os.path.splitext(os.path.basename(
-                    service_overlap_raster_path))
-                )
-            vector_info = geoprocessing.get_vector_info(PROTECTED_AREAS[region_id])
-            raster_info = geoprocessing.get_raster_info(service_overlap_raster_path);
+                    service_overlap_raster_path)))
             pa_overlap_task = task_graph.add_task(
                 func=geoprocessing.mask_raster,
                 args=(
@@ -127,8 +93,7 @@ def main():
             service_overlap_in_kba_path = os.path.join(
                 WORKING_DIR,
                 f'%s_{region_id}_in_kba%s' % os.path.splitext(os.path.basename(
-                    service_overlap_raster_path))
-                )
+                    service_overlap_raster_path)))
             kba_overlap_task = task_graph.add_task(
                 func=geoprocessing.mask_raster,
                 args=(
@@ -145,8 +110,7 @@ def main():
             service_overlap_in_pa_excluding_kba_path = os.path.join(
                 WORKING_DIR,
                 f'%s_{region_id}_in_kba_excluding_pa%s' % os.path.splitext(
-                    os.path.basename(service_overlap_raster_path))
-                )
+                    os.path.basename(service_overlap_raster_path)))
             kba_excluding_pa_overlap_task = task_graph.add_task(
                 func=exclude_by_raster,
                 args=(
@@ -157,58 +121,67 @@ def main():
                 dependent_task_list=[kba_overlap_task, pa_overlap_task],
                 task_name=f'exclude PA from KBA for {region_id}')
 
-            pixel_counts = {}
+            raster_area = {}
             for key, raster_path, dependent_task_list in [
                     ('service', service_overlap_raster_path, []),
                     ('service_in_pa', service_overlap_in_pa_path, [pa_overlap_task]),
                     ('service_in_kpa', service_overlap_in_kba_path, [kba_overlap_task]),
                     ('service_in_kpa_excluding_pa', service_overlap_in_pa_excluding_kba_path, [kba_excluding_pa_overlap_task])]:
-                pixel_counts[key] = task_graph.add_task(
-                    func=count_valid_pixels,
-                    args=(raster_path,),
+                raster_area[key] = task_graph.add_task(
+                    func=calculate_pixel_area_km2,
+                    args=(raster_path, ELLIPSOID_EPSG),
                     dependent_task_list=dependent_task_list,
                     store_result=True,
                     task_name=f'count valid in {region_id} {key}')
             service_overlap_basename = os.path.basename(
                 os.path.splitext(service_overlap_raster_path)[0])
-            pixel_counts_per_region[
-                f'{region_id}_{service_overlap_basename}'] = pixel_counts
+            raster_area_per_region[
+                f'{region_id}_{service_overlap_basename}'] = raster_area
 
     task_graph.join()
     task_graph.close()
     pixel_count_file = open(os.path.join(
         RESULTS_DIR, 'area_analysis_of_priority_areas_v2.csv'), 'w')
-    for region_id, pixel_counts in pixel_counts_per_region.items():
+    for region_id, raster_area_ in raster_area_per_region.items():
         pixel_count_file.write(f'{region_id}\n')
-        pixel_count_file.write(f'service,area in Ha,% of total service\n')
+        pixel_count_file.write(f'service,area in km2,% of total service\n')
         first_val = None
-        for service_key, task in pixel_counts.items():
-            _, area_in_ha = task.get()
+        for service_key, task in raster_area_.items():
+            area_in_km2 = task.get()
             if first_val is None:
-                first_val = area_in_ha
-            pixel_count_file.write(f'{service_key},{area_in_ha:.1f},{area_in_ha/first_val*100:.2f}%\n')
+                first_val = area_in_km2
+            pixel_count_file.write(f'{service_key},{area_in_km2:.1f},{area_in_km2/first_val*100:.2f}%\n')
     pixel_count_file.close()
     LOGGER.info(f'all done, results at {RESULTS_DIR}')
 
 
-def count_valid_pixels(raster_path):
-    raster_info = geoprocessing.get_raster_info(raster_path)
-    nodata = raster_info['nodata'][0]
-    gt = raster_info['geotransform']
+def calculate_pixel_area_km2(base_raster_path, target_epsg):
+    source_raster = gdal.OpenEx(base_raster_path, gdal.OF_RASTER)
+    target_srs = osr.SpatialReference()
+    target_srs.ImportFromEPSG(target_epsg)
 
-    valid_count = 0
-    total_area_ha = 0
-    for offset_dict, array in geoprocessing.iterblocks((raster_path, 1)):
-        x_coord, y_coord = gdal.ApplyGeoTransform(
-            gt,
-            offset_dict['xoff']+offset_dict['win_xsize']/2,
-            offset_dict['yoff']+offset_dict['win_ysize']/2)
-        pixel_area_in_ha = 100*area_of_pixel_km2(raster_info['pixel_size'][0], y_coord)
-        local_valid_count = numpy.count_nonzero(
-            (array != nodata) & (array != 0.0))
-        valid_count += local_valid_count
-        total_area_ha += pixel_area_in_ha*local_valid_count
-    return valid_count, total_area_ha
+    LOGGER.debug(f'about to warp {base_raster_path} to epsg:{target_epsg}')
+    reprojected_raster = gdal.Warp(
+        '',
+        source_raster,
+        format='MEM',
+        dstSRS=target_srs)
+    reprojected_band = reprojected_raster.GetRasterBand(1)
+    nodata = reprojected_band.GetNoDataValue()
+    LOGGER.debug(f'nodata value is {nodata}')
+    LOGGER.debug(f'read warped {base_raster_path}')
+    reprojected_data = reprojected_band.ReadAsArray()
+    reprojected_geotransform = reprojected_raster.GetGeoTransform()
+    reprojected_pixel_width, reprojected_pixel_height = (
+        reprojected_geotransform[1], abs(reprojected_geotransform[5]))
+
+    # Calculate the area of pixels with values > 1
+    pixel_area = reprojected_pixel_width * reprojected_pixel_height
+    count = ((reprojected_data > 0) & (reprojected_data != nodata)).sum()
+    total_area = count * pixel_area / 1e6  # covert to km2
+
+    LOGGER.debug(f'total area of {base_raster_path}: {total_area}km2')
+    return total_area
 
 
 def exclude_by_raster(
