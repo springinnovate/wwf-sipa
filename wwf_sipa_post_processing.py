e4416345377cd711e4b19a0385e39a0a66c259a3
Richard P Sharp Jr <richpsharp@gmail.com>
2024-11-03 17:11:15 -0800

    we don't have a worstcase ssp245, just worstcase

--- wwf_sipa_post_processing.py
+++ wwf_sipa_post_processing.py
@@ -447,7 +447,7 @@ def main():
             DIFF_SEDIMENT_IDN_CONSERVATION_ALL
         ),
         (
-            r"D:\repositories\ndr_sdr_global\wwf_IDN_worstcase_ssp245_climate\stitched_sed_export_wwf_IDN_worstcase_ssp245_climate.tif",
+            r"D:\repositories\ndr_sdr_global\wwf_IDN_worstcase_historical_climate\stitched_sed_export_wwf_IDN_worstcase_historical_climate.tif",
             r"D:\repositories\ndr_sdr_global\wwf_IDN_baseline_ssp245_climate\stitched_sed_export_wwf_IDN_baseline_ssp245_climate.tif",
             DIFF_SEDIMENT_IDN_CONSERVATION_ALL_SSP245
         ),
@@ -477,10 +477,10 @@ def main():
             DIFF_SEDIMENT_PH_CONSERVATION_ALL
         ),
         (
-            r"D:\repositories\ndr_sdr_global\wwf_PH_worstcase_ssp245_climate\stitched_sed_export_wwf_PH_worstcase_ssp245_climate.tif",
+            r"D:\repositories\ndr_sdr_global\wwf_PH_worstcase_historical_climate\stitched_sed_export_wwf_PH_worstcase_historical_climate.tif",
             r"D:\repositories\ndr_sdr_global\wwf_PH_baseline_ssp245_climate\stitched_sed_export_wwf_PH_baseline_ssp245_climate.tif",
             DIFF_SEDIMENT_PH_CONSERVATION_ALL_SSP245
-        ),
+        )
 
         (r"D:\repositories\swy_global\workspace_swy_wwf_IDN_restoration_historical_climate\B_wwf_IDN_restoration_historical_climate.tif",
          r"D:\repositories\swy_global\workspace_swy_wwf_IDN_baseline_historical_climate\B_wwf_IDN_baseline_historical_climate.tif",
